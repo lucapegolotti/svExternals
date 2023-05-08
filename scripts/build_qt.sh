f7c116b0d16e1efe412e4170f0674efd7616d47d
Luca Pegolotti <pegolotti.luca@gmail.com>
2023-05-08 15:24:03 -0700

    Remove rm

--- scripts/build_qt.sh
+++ scripts/build_qt.sh
@@ -11,8 +11,6 @@ then
     QT_MAJOR_VERSION=${QT_VERSION%.*}
     wget https://download.qt.io/archive/qt/$QT_MAJOR_VERSION/$QT_VERSION/single/qt-everywhere-src-$QT_VERSION.tar.xz
 
-
-    rm -r qt-everywhere-src-5.14.2
     # extract source
     tar -xf qt-everywhere-src-$QT_VERSION.tar.xz
 
