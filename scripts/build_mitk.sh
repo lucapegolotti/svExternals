6211e0b87a3d7b8ddf513eb5d26b0cb35b0d3ef9
Luca Pegolotti <pegolotti.luca@gmail.com>
2023-05-31 01:26:07 -0700

    Add echo statements to mitk script for debug

--- scripts/build_mitk.sh
+++ scripts/build_mitk.sh
@@ -90,6 +90,8 @@ popd
 
 popd
 
+echo 'Finished Install step'
+
 # below are the content of the post-install script previously in SVExternals.
 # I am not sure why we need to change the build directory so much
 
@@ -111,6 +113,8 @@ export MITK_BLDTYPE=
 
 # primary directories to install into
 
+echo '1'
+
 $GMKDIR -p $MITK_BINDIR/bin
 $GMKDIR -p $MITK_BINDIR/lib
 $GMKDIR -p $MITK_BINDIR/lib/plugins
@@ -131,6 +135,7 @@ $GCP -Rfl $MITK_BLDDIR/ep/src/CTK-build/CTK-build/bin/* $MITK_BINDIR/bin 2>/dev/
 $GCP -fl -d $MITK_BLDDIR/ep/src/CTK-build/CTK-build/bin/$MITK_BLDTYPE/*CTK*.so* $MITK_BINDIR/lib 2>/dev/null
 $GCP -fl -d $MITK_BLDDIR/ep/src/CTK-build/CTK-build/bin/$MITK_BLDTYPE/liborg*.so* $MITK_BINDIR/lib/plugins 2>/dev/null
 
+echo '2'
 
 # copy qRestAPI from CTK-build
 
@@ -139,6 +144,8 @@ $GCP -fl $MITK_BLDDIR/ep/src/CTK-build/qRestAPI/*.h $MITK_BINDIR/include/qRestAP
 $GCP -fl $MITK_BLDDIR/ep/src/CTK-build/qRestAPI-build/$MITK_BLDTYPE/libqRestAPI.REPLACEME_SV_LIB_FILE_EXTENSION $MITK_BINDIR/lib 2>/dev/null
 $GCP -fl -d $MITK_BLDDIR/ep/src/CTK-build/qRestAPI-build/$MITK_BLDTYPE/libqRestAPI.so* $MITK_BINDIR/bin 2>/dev/null
 $GCP -fl -d $MITK_BLDDIR/ep/src/CTK-build/qRestAPI-build/$MITK_BLDTYPE/libqRestAPI.so* $MITK_BINDIR/lib 2>/dev/null
+
+echo '3'
 
 # copy PythonQt from CTK-build
 
@@ -149,11 +156,14 @@ $GCP -fl $MITK_BLDDIR/ep/src/CTK-build/PythonQt-build/$MITK_BLDTYPE/libPythonQt.
 $GCP -fl -d $MITK_BLDDIR/ep/src/CTK-build/PythonQt-build/$MITK_BLDTYPE/libPythonQt.so* $MITK_BINDIR/bin 2>/dev/null
 $GCP -fl -d $MITK_BLDDIR/ep/src/CTK-build/PythonQt-build/$MITK_BLDTYPE/libPythonQt.so* $MITK_BINDIR/lib 2>/dev/null
 
+echo '4'
+
 # remove libs from $MITK_BINDIR/bin
 $GRM -Rf  $MITK_BINDIR/bin/*.so*
 $GRM -Rf  $MITK_BINDIR/bin/plugins
 
 # CTK
+echo '5'
 
 $GMV -f $MITK_BINDIR/bin/Python $MITK_BINDIR/bin/PythonCTK
 
@@ -165,6 +175,8 @@ $GCP -fl $MITK_BLDDIR/ep/src/CTK/Libs/Scripting/Python/Core/*.h $MITK_BINDIR/inc
 $GCP -fl $MITK_BLDDIR/ep/src/CTK/Libs/Scripting/Python/Widgets/*.h $MITK_BINDIR/include/ctk 2>/dev/null
 $GCP -fl $MITK_BLDDIR/ep/src/CTK/Libs/Visualization/VTK/Core/*.h $MITK_BINDIR/include/ctk 2>/dev/null
 $GCP -fl $MITK_BLDDIR/ep/src/CTK/Libs/Widgets/*.h $MITK_BINDIR/include/ctk
+
+echo '6'
 
 # $GCP -fl -d $MITK_BLDDIR/ep/src/CTK-build/CTK-build/bin/$MITK_BLDTYPE/*.so* $MITK_BINDIR/bin
 # $GCP -fl -d $MITK_BLDDIR/ep/src/CTK-build/CTK-build/bin/$MITK_BLDTYPE/*.so* $MITK_BINDIR/lib
@@ -178,6 +190,8 @@ for i in $(find $MITK_BLDDIR/ep/src/CTK-build -name "*Export.h"); do
     $GCP -fl $i $MITK_BINDIR/include/ctk
 done
 
+echo '7'
+
 #$GCP -fl $MITK_BLDDIR/MITK-build/lib/plugins/$MITK_BLDTYPE/* $MITK_BINDIR/lib/plugins
 
 # mitk files
@@ -186,6 +200,8 @@ done
 #$GCP -fl -d $MITK_BLDDIR/MITK-build/lib/$MITK_BLDTYPE/*.so* $MITK_BINDIR/lib
 #$GCP -fl $MITK_BLDDIR/MITK-build/lib/$MITK_BLDTYPE/*.REPLACEME_SV_LIB_FILE_EXTENSION $MITK_BINDIR/lib
 
+echo '8'
+
 $GMKDIR -p $MITK_BINDIR/include/mitk
 $GMKDIR -p $MITK_BINDIR/include/mitk/configs
 $GMKDIR -p $MITK_BINDIR/include/mitk/exports
@@ -193,6 +209,8 @@ $GMKDIR -p $MITK_BINDIR/include/mitk/ui_files
 $GMKDIR -p $MITK_BINDIR/include/mitk/Modules
 
 $GCP $MITK_BLDDIR/MITK-build/*.h $MITK_BINDIR/include/mitk 2>/dev/null
+
+echo '9'
 
 #
 #  plugins
@@ -237,6 +255,8 @@ for i in $(find $MITK_BLDDIR/MITK-build/Plugins -name "*Export.h"); do
     $GCP -fl $i $MITK_BINDIR/include/mitk/exports 2>/dev/null
 done
 
+echo '10'
+
 #
 # everything else
 #
@@ -276,70 +296,82 @@ for i in $(find $MITK_BLDDIR/MITK-build -name "*Config.h"); do
     $GCP -fl $i $MITK_BINDIR/include/mitk/configs 2>/dev/null
 done
 
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/ContourModel/DataManagement
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/CppMicroServices/core/src/module
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/CppMicroServices/core/src/service
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/CppMicroServices/core/src/util
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/ImageDenoising
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/LegacyGL
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Multilabel
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Overlays
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Segmentation/Algorithms
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Segmentation/Controllers
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Segmentation/Interactions
-	   $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Segmentation/SegmentationUtilities/BooleanOperations
-	   $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Segmentation/SegmentationUtilities/MorphologicalOperations
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/SegmentationUI/Qmitk
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/SurfaceInterpolation
+echo '11'
 
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/ContourModel
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/ImageDenoising
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/LegacyGL
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Multilabel
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Overlays
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/QtWidgets
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Segmentation
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Segmentation/Interactions
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/SegmentationUI
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Modules/SurfaceInterpolation
-           $GMKDIR -p $MITK_BINDIR/include/mitk/Utilities/mbilog
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/ContourModel/DataManagement
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/CppMicroServices/core/src/module
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/CppMicroServices/core/src/service
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/CppMicroServices/core/src/util
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/ImageDenoising
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/LegacyGL
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Multilabel
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Overlays
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Segmentation/Algorithms
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Segmentation/Controllers
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Segmentation/Interactions
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Segmentation/SegmentationUtilities/BooleanOperations
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Segmentation/SegmentationUtilities/MorphologicalOperations
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/SegmentationUI/Qmitk
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/SurfaceInterpolation
 
-	   $GCP -fl $MITK_SRCDIR/Modules/ContourModel/DataManagement/*.h $MITK_BINDIR/include/mitk/Modules/ContourModel/DataManagement 2>/dev/null
-           $GCP -fl $MITK_SRCDIR/Modules/CppMicroServices/core/src/module/*.h $MITK_BINDIR/include/mitk/Modules/CppMicroServices/core/src/module 2>/dev/null
-           $GCP -fl $MITK_SRCDIR/Modules/CppMicroServices/core/src/service/*.h $MITK_BINDIR/include/mitk/Modules/CppMicroServices/core/src/service 2>/dev/null
-           $GCP -fl $MITK_SRCDIR/Modules/CppMicroServices/core/src/util/*.h $MITK_BINDIR/include/mitk/Modules/CppMicroServices/core/src/util 2>/dev/null
-	   $GCP -fl $MITK_SRCDIR/Modules/CppMicroServices/core/src/module/*.tpp $MITK_BINDIR/include/mitk/Modules/CppMicroServices/core/src/module 2>/dev/null
-           $GCP -fl $MITK_SRCDIR/Modules/CppMicroServices/core/src/service/*.tpp $MITK_BINDIR/include/mitk/Modules/CppMicroServices/core/src/service 2>/dev/null
-           $GCP -fl $MITK_SRCDIR/Modules/CppMicroServices/core/src/util/*.tpp $MITK_BINDIR/include/mitk/Modules/CppMicroServices/core/src/util 2>/dev/null
-           $GCP -fl $MITK_SRCDIR/Modules/ImageDenoising/*.h $MITK_BINDIR/include/mitk/Modules/ImageDenoising 2>/dev/null
-	   $GCP -fl $MITK_SRCDIR/Modules/ImageDenoising/*.txx $MITK_BINDIR/include/mitk/Modules/ImageDenoising 2>/dev/null
-           $GCP -fl $MITK_SRCDIR/Modules/LegacyGL/*.h $MITK_BINDIR/include/mitk/Modules/LegacyGL 2>/dev/null
-           $GCP -fl $MITK_SRCDIR/Modules/Multilabel/*.h $MITK_BINDIR/include/mitk/Modules/Multilabel 2>/dev/null
-           $GCP -fl $MITK_SRCDIR/Modules/Overlays/*.h $MITK_BINDIR/include/mitk/Modules/Overlays 2>/dev/null
-           $GCP -fl $MITK_SRCDIR/Modules/Segmentation/Algorithms/*.h $MITK_BINDIR/include/mitk/Modules/Segmentation/Algorithms 2>/dev/null
-           $GCP -fl $MITK_SRCDIR/Modules/Segmentation/Controllers/*.h $MITK_BINDIR/include/mitk/Modules/Segmentation/Controllers 2>/dev/null
-           $GCP -fl $MITK_SRCDIR/Modules/Segmentation/Interactions/*.h $MITK_BINDIR/include/mitk/Modules/Segmentation/Interactions 2>/dev/null
-	   $GCP -fl $MITK_SRCDIR/Modules/Segmentation/SegmentationUtilities/BooleanOperations/*.h $MITK_BINDIR/include/mitk/Modules/Segmentation/SegmentationUtilities/BooleanOperations 2>/dev/null
-	   $GCP -fl $MITK_SRCDIR/Modules/Segmentation/SegmentationUtilities/MorphologicalOperations/*.h $MITK_BINDIR/include/mitk/Modules/Segmentation/SegmentationUtilities/MorphologicalOperations
-           $GCP -fl $MITK_SRCDIR/Modules/SegmentationUI/Qmitk/*.h $MITK_BINDIR/include/mitk/Modules/SegmentationUI/Qmitk 2>/dev/null
-           $GCP -fl $MITK_SRCDIR/Modules/SurfaceInterpolation/*.h $MITK_BINDIR/include/mitk/Modules/SurfaceInterpolation 2>/dev/null
-           $GCP -fl $MITK_SRCDIR/Utilities/mbilog/*.h $MITK_BINDIR/include/mitk/Utilities/mbilog 2>/dev/null
+echo '12'
 
-           $GCP -fl $MITK_BLDDIR/MITK-build/Modules/ContourModel/*.h $MITK_BINDIR/include/mitk/Modules/ContourModel 2>/dev/null
-           $GCP -fl $MITK_BLDDIR/MITK-build/Modules/ImageDenoising/*.h $MITK_BINDIR/include/mitk/Modules/ImageDenoising 2>/dev/null
-           $GCP -fl $MITK_BLDDIR/MITK-build/Modules/LegacyGL/*.h $MITK_BINDIR/include/mitk/Modules/LegacyGL 2>/dev/null
-           $GCP -fl $MITK_BLDDIR/MITK-build/Modules/Multilabel/*.h $MITK_BINDIR/include/mitk/Modules/Multilabel 2>/dev/null
-           $GCP -fl $MITK_BLDDIR/MITK-build/Modules/Overlays/*.h $MITK_BINDIR/include/mitk/Modules/Overlays 2>/dev/null
-           $GCP -fl $MITK_BLDDIR/MITK-build/Modules/QtWidgets/*.h $MITK_BINDIR/include/mitk/Modules/QtWidgets 2>/dev/null
-           $GCP -fl $MITK_BLDDIR/MITK-build/Modules/Segmentation/*.h $MITK_BINDIR/include/mitk/Modules/Segmentation 2>/dev/null
-           $GCP -fl $MITK_BLDDIR/MITK-build/Modules/Segmentation/Interactions/*.h $MITK_BINDIR/include/mitk/Modules/Segmentation/Interactions 2>/dev/null
-           $GCP -fl $MITK_BLDDIR/MITK-build/Modules/SegmentationUI/*.h $MITK_BINDIR/include/mitk/Modules/SegmentationUI 2>/dev/null
-           $GCP -fl $MITK_BLDDIR/MITK-build/Modules/SurfaceInterpolation/*.h $MITK_BINDIR/include/mitk/Modules/SurfaceInterpolation 2>/dev/null
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/ContourModel
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/ImageDenoising
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/LegacyGL
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Multilabel
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Overlays
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/QtWidgets
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Segmentation
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/Segmentation/Interactions
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/SegmentationUI
+$GMKDIR -p $MITK_BINDIR/include/mitk/Modules/SurfaceInterpolation
+$GMKDIR -p $MITK_BINDIR/include/mitk/Utilities/mbilog
+
+echo '13'
+
+$GCP -fl $MITK_SRCDIR/Modules/ContourModel/DataManagement/*.h $MITK_BINDIR/include/mitk/Modules/ContourModel/DataManagement 2>/dev/null
+$GCP -fl $MITK_SRCDIR/Modules/CppMicroServices/core/src/module/*.h $MITK_BINDIR/include/mitk/Modules/CppMicroServices/core/src/module 2>/dev/null
+$GCP -fl $MITK_SRCDIR/Modules/CppMicroServices/core/src/service/*.h $MITK_BINDIR/include/mitk/Modules/CppMicroServices/core/src/service 2>/dev/null
+$GCP -fl $MITK_SRCDIR/Modules/CppMicroServices/core/src/util/*.h $MITK_BINDIR/include/mitk/Modules/CppMicroServices/core/src/util 2>/dev/null
+$GCP -fl $MITK_SRCDIR/Modules/CppMicroServices/core/src/module/*.tpp $MITK_BINDIR/include/mitk/Modules/CppMicroServices/core/src/module 2>/dev/null
+$GCP -fl $MITK_SRCDIR/Modules/CppMicroServices/core/src/service/*.tpp $MITK_BINDIR/include/mitk/Modules/CppMicroServices/core/src/service 2>/dev/null
+$GCP -fl $MITK_SRCDIR/Modules/CppMicroServices/core/src/util/*.tpp $MITK_BINDIR/include/mitk/Modules/CppMicroServices/core/src/util 2>/dev/null
+$GCP -fl $MITK_SRCDIR/Modules/ImageDenoising/*.h $MITK_BINDIR/include/mitk/Modules/ImageDenoising 2>/dev/null
+$GCP -fl $MITK_SRCDIR/Modules/ImageDenoising/*.txx $MITK_BINDIR/include/mitk/Modules/ImageDenoising 2>/dev/null
+$GCP -fl $MITK_SRCDIR/Modules/LegacyGL/*.h $MITK_BINDIR/include/mitk/Modules/LegacyGL 2>/dev/null
+$GCP -fl $MITK_SRCDIR/Modules/Multilabel/*.h $MITK_BINDIR/include/mitk/Modules/Multilabel 2>/dev/null
+$GCP -fl $MITK_SRCDIR/Modules/Overlays/*.h $MITK_BINDIR/include/mitk/Modules/Overlays 2>/dev/null
+$GCP -fl $MITK_SRCDIR/Modules/Segmentation/Algorithms/*.h $MITK_BINDIR/include/mitk/Modules/Segmentation/Algorithms 2>/dev/null
+$GCP -fl $MITK_SRCDIR/Modules/Segmentation/Controllers/*.h $MITK_BINDIR/include/mitk/Modules/Segmentation/Controllers 2>/dev/null
+$GCP -fl $MITK_SRCDIR/Modules/Segmentation/Interactions/*.h $MITK_BINDIR/include/mitk/Modules/Segmentation/Interactions 2>/dev/null
+$GCP -fl $MITK_SRCDIR/Modules/Segmentation/SegmentationUtilities/BooleanOperations/*.h $MITK_BINDIR/include/mitk/Modules/Segmentation/SegmentationUtilities/BooleanOperations 2>/dev/null
+$GCP -fl $MITK_SRCDIR/Modules/Segmentation/SegmentationUtilities/MorphologicalOperations/*.h $MITK_BINDIR/include/mitk/Modules/Segmentation/SegmentationUtilities/MorphologicalOperations
+$GCP -fl $MITK_SRCDIR/Modules/SegmentationUI/Qmitk/*.h $MITK_BINDIR/include/mitk/Modules/SegmentationUI/Qmitk 2>/dev/null
+$GCP -fl $MITK_SRCDIR/Modules/SurfaceInterpolation/*.h $MITK_BINDIR/include/mitk/Modules/SurfaceInterpolation 2>/dev/null
+$GCP -fl $MITK_SRCDIR/Utilities/mbilog/*.h $MITK_BINDIR/include/mitk/Utilities/mbilog 2>/dev/null
+
+echo '14'
+
+$GCP -fl $MITK_BLDDIR/MITK-build/Modules/ContourModel/*.h $MITK_BINDIR/include/mitk/Modules/ContourModel 2>/dev/null
+$GCP -fl $MITK_BLDDIR/MITK-build/Modules/ImageDenoising/*.h $MITK_BINDIR/include/mitk/Modules/ImageDenoising 2>/dev/null
+$GCP -fl $MITK_BLDDIR/MITK-build/Modules/LegacyGL/*.h $MITK_BINDIR/include/mitk/Modules/LegacyGL 2>/dev/null
+$GCP -fl $MITK_BLDDIR/MITK-build/Modules/Multilabel/*.h $MITK_BINDIR/include/mitk/Modules/Multilabel 2>/dev/null
+$GCP -fl $MITK_BLDDIR/MITK-build/Modules/Overlays/*.h $MITK_BINDIR/include/mitk/Modules/Overlays 2>/dev/null
+$GCP -fl $MITK_BLDDIR/MITK-build/Modules/QtWidgets/*.h $MITK_BINDIR/include/mitk/Modules/QtWidgets 2>/dev/null
+$GCP -fl $MITK_BLDDIR/MITK-build/Modules/Segmentation/*.h $MITK_BINDIR/include/mitk/Modules/Segmentation 2>/dev/null
+$GCP -fl $MITK_BLDDIR/MITK-build/Modules/Segmentation/Interactions/*.h $MITK_BINDIR/include/mitk/Modules/Segmentation/Interactions 2>/dev/null
+$GCP -fl $MITK_BLDDIR/MITK-build/Modules/SegmentationUI/*.h $MITK_BINDIR/include/mitk/Modules/SegmentationUI 2>/dev/null
+$GCP -fl $MITK_BLDDIR/MITK-build/Modules/SurfaceInterpolation/*.h $MITK_BINDIR/include/mitk/Modules/SurfaceInterpolation 2>/dev/null
+
+echo '15'
 
 # copy executable
 $GCP -fl $MITK_BLDDIR/MITK-build/bin/MitkWorkbench* $MITK_BINDIR/bin 2>/dev/null
 $GCP -fl $MITK_BLDDIR/MITK-build/bin/usResourceCompiler* $MITK_BINDIR/bin 2>/dev/null
 $GCP -fl $MITK_BLDDIR/MITK-build/bin/MitkPluginGenerator* $MITK_BINDIR/bin 2>/dev/null
+
+echo '16'
 
 # for i in $(find $MITK_BLDDIR/MITK-build/lib/plugins -name "*.so*"); do
 #     echo "$i  $($GBASENAME $i)"
@@ -365,3 +397,5 @@ chmod u+w,a+rx ${MITK_INSTALL_DIR}/bin/workbench-wrapper
 cd install
 tar -cf mitk.tar mitk-$MITK_VERSION
 cd ..
+
+echo '17'
