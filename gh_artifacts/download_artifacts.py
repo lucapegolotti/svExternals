3d7ad6364288feb44af4c4fa2d378b2847046cd4
Luca Pegolotti <pegolotti.luca@gmail.com>
2023-06-14 08:57:52 -0700

    Fix issue in opencascade

--- gh_artifacts/download_artifacts.py
+++ gh_artifacts/download_artifacts.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-gh_token = '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'
+gh_token = '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'
 gh_repo = 'https://api.github.com/repos/lucapegolotti/SVExternals/actions/artifacts'
 
 zip_prefix = 'ubuntu.20.04.gnu.7.5.x64.release.2022.10.13.'
